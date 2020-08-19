03a8f87062c65702f4096c0f3485adb90bbc6c64
keelia <xiaoyuetang2017@gmail.com>
2020-08-19 14:49:45 +0800

    - part1 as class

--- src/part1.py
+++ src/part1.py
@@ -4,124 +4,105 @@ from pprint import pprint
 import numpy as np
 import os
 
-def smoothedEstimation(tags,words,labelWords,k):
-    emissionPrbability = {}
-    for tag in labelWords:
-        emissionPrbability[tag] = {}
-        # labelWords[tag]['#UNK#'] = 0
-        for word in list(labelWords[tag]):
-            if word not in words:
-                labelWords[tag]['#UNK#'] += labelWords[tag].pop(word)
-            elif words[word] < k:
-                labelWords[tag]['#UNK#'] += labelWords[tag].pop(word)
-                del words[word]
-            else:
+class Feature():
+    def __init__(self, path_train):
+        self.path_train = path_train
+        self.train_data, self.train_x, self.train_y = self.generate_data(self.path_train)
+        self.tags, self.words, self.labelWords = self.dataProcessing(self.path_train)
+        self.emission_para = self.calculate_emiss_parameter(self.tags, self.words, self.labelWords)
+        self.transition_para = self.calculate_trans_parameter(self.path_train)
+        self.feature_dict = self.calculate_feature(self.emission_para, self.transition_para)
+
+    def calculate_emiss_parameter(self, tags, words, labelWords):
+        emissionPrbability = {}
+        for tag in labelWords:
+            emissionPrbability[tag] = {}
+            for word in list(labelWords[tag]):
                 emissionPrbability[tag][word] = labelWords[tag][word] / tags[tag]
-        # emissionPrbability[tag]['#UNK#'] = labelWords[tag]['#UNK#'] / tags[tag]
-    trainFile= list(words)
-    return trainFile,emissionPrbability
+        return emissionPrbability
 
+    def calculate_trans_parameter(self, filePath):
+        tags = {}
+        transitionTag = {}
+        transitionProbability = {}
+        _preT = ''
+        _newT = 'START'
+        for line in open(filePath, encoding='utf-8', mode='r'):
+            _preT = _newT if (_newT != 'STOP') else 'START'
+            segmentedLine = line.rstrip()
 
-def calculate_trans_parameter(filePath):
-    tags = {}
-    transitionTag = {}
-    transitionProbability = {}
-    _preT = ''
-    _newT = 'START'
-    for line in open(filePath, encoding='utf-8', mode='r'):
-        _preT = _newT if (_newT != 'STOP') else 'START'
-        segmentedLine = line.rstrip()
-
-        if segmentedLine:
-            segmentedLine = segmentedLine.rsplit(' ', 1)
-            _newT = segmentedLine[1]
-        else:
-            _newT = 'STOP'
-        if _preT not in tags:
-            tags[_preT] = 1
-            transitionTag[_preT] = {_newT: 1}
-        else:
-            tags[_preT] += 1
-            if _newT not in transitionTag[_preT]:
-                transitionTag[_preT][_newT] = 1
+            if segmentedLine:
+                segmentedLine = segmentedLine.rsplit(' ', 1)
+                _newT = segmentedLine[1]
             else:
-                transitionTag[_preT][_newT] += 1
-
-    calculateTransitionProbability(transitionProbability, transitionTag, tags)
-
-    return transitionProbability
-
-
-def calculateTransitionProbability(transitionProbability, transitionTag, tags):
-    for tag in transitionTag:
-        transitionProbability[tag] = {}
-        for transition in transitionTag[tag]:
-            transitionProbability[tag][transition] = transitionTag[tag][transition] / tags[tag]
-
-
-def calculate_feature(emission_parameter, transition_parameter):
-    feature_dic = defaultdict()
-
-    # for word in emission_parameter.keys():
-    #     for idx, tag in enumerate(emission_parameter[word][0]):
-    #         feature_dic["emission:" + tag + '+' + word] = np.log2(emission_parameter[word][1][idx])
-    for tag in emission_parameter.keys():
-        for word in emission_parameter[tag]:
-            feature_dic["emission:" + tag + "+" +word] = np.log2(emission_parameter[tag][word])
-    for word in transition_parameter.keys():
-        for word2 in transition_parameter[word]:
-            feature_dic["transition:" + word + '+' + word2] = np.log2(transition_parameter[word][word2])
-
-    return feature_dic
-
-
-def dataProcessing(filePath):
-    tags = {}
-    words = {}
-    labelWords = {}
-
-    for line in open(filePath, encoding='utf-8', mode='r'):
-        segmentedLine = line.strip()
-        if segmentedLine:
-            word, tag = lineCut(segmentedLine)
-            if word not in words:
-                words[word] = 1
+                _newT = 'STOP'
+            if _preT not in tags:
+                tags[_preT] = 1
+                transitionTag[_preT] = {_newT: 1}
             else:
-                words[word] += 1
-            if tag not in tags:
-                tags[tag] = 1
-                labelWords[tag] = {word: 1}
-            else:
-                tags[tag] += 1
-                if word not in labelWords[tag]:
-                    labelWords[tag][word] = 1
+                tags[_preT] += 1
+                if _newT not in transitionTag[_preT]:
+                    transitionTag[_preT][_newT] = 1
                 else:
-                    labelWords[tag][word] += 1
+                    transitionTag[_preT][_newT] += 1
 
-    return tags, words, labelWords
+        for tag in transitionTag:
+            transitionProbability[tag] = {}
+            for transition in transitionTag[tag]:
+                transitionProbability[tag][transition] = transitionTag[tag][transition] / tags[tag]
 
+        return transitionProbability
 
-def lineCut(segmentedLine):
-    segmentedLine = segmentedLine.rsplit(' ', 1)
-    word = segmentedLine[0]
-    tag = segmentedLine[1]
-    return word,tag
+    def calculate_feature(self, emission_parameter, transition_parameter):
+        feature_dic = defaultdict()
 
+        for tag in emission_parameter.keys():
+            for word in emission_parameter[tag]:
+                feature_dic["emission:" + tag + "+" +word] = np.log2(emission_parameter[tag][word])
+        for word in transition_parameter.keys():
+            for word2 in transition_parameter[word]:
+                feature_dic["transition:" + word + '+' + word2] = np.log2(transition_parameter[word][word2])
 
-def generate_data(path_train):
-    train_lines = list(filter(None, open(path_train).read().splitlines()))
-    train_data = [line.split() for line in train_lines]
-    train_x = [line[0] for line in train_data if line]
-    train_y = [line[1] for line in train_data if line]
+        return feature_dic
+    def generate_data(self, path_train):
+        train_lines = list(filter(None, open(path_train).read().splitlines()))
 
-    # # emission_para = calculate_emission_parameter(train_data, train_x, train_y)
-    # tags,words,labelWords= dataProcessing(path_train)
-    # trainFile,emission_para = smoothedEstimation(tags,words,labelWords,k=-1)
-    # transition_para = calculate_trans_parameter(path_train)
-    # # print(emission_para)
-    # feature = calculate_feature(emission_para, transition_para)
-    # print(feature)
-    return train_data, train_x, train_y
+        train_data = [line.split() for line in train_lines]
+        train_x = [line[0] for line in train_data if line]
+        train_y = [line[1] for line in train_data if line]
+
+        return train_data, train_x, train_y
+
+    def dataProcessing(self, filePath):
+        tags = {}
+        words = {}
+        labelWords = {}
+
+        for line in open(filePath, encoding='utf-8', mode='r'):
+            segmentedLine = line.strip()
+            if segmentedLine:
+                word, tag = self.lineCut(segmentedLine)
+                if word not in words:
+                    words[word] = 1
+                else:
+                    words[word] += 1
+                if tag not in tags:
+                    tags[tag] = 1
+                    labelWords[tag] = {word: 1}
+                else:
+                    tags[tag] += 1
+                    if word not in labelWords[tag]:
+                        labelWords[tag][word] = 1
+                    else:
+                        labelWords[tag][word] += 1
+
+        return tags, words, labelWords
+
+    def lineCut(self, segmentedLine):
+        segmentedLine = segmentedLine.rsplit(' ', 1)
+        word = segmentedLine[0]
+        tag = segmentedLine[1]
+        return word, tag
 
 
 def take(n, iterable):
@@ -129,15 +110,8 @@ def take(n, iterable):
     return list(islice(iterable, n))
 
 
-path_train = os.path.join(os.path.dirname( __file__ ),"..","data","partial","train")
-train_data, train_x, train_y = generate_data(path_train)
-
-tags, words, labelWords = dataProcessing(path_train)
-trainFile, emission_para = smoothedEstimation(tags, words, labelWords, k=-1)
-transition_para = calculate_trans_parameter(path_train)
-feature_dict = calculate_feature(emission_para, transition_para)
-
-
 if __name__ == '__main__':
+    feature = Feature(os.path.join(os.path.dirname( __file__ ),"..","data","partial","train"))
+    feature_dict = feature.feature_dict
     n_items = take(5, feature_dict.items())
     pprint(n_items)
--- src/part2.py
+++ src/part2.py
@@ -6,47 +6,175 @@ import os
 from part1 import feature_dict, train_x, train_y
 
 
-def feature_function(sequence_pair):
-    '''
-    a function that returns the number of times that the j-th feature in the
-    feature_dictionary appears in sequence pair (x_seq, y_seq).
-    '''
-    count_dict = dict()
-    for feature, weight in feature_dict.items():
-        feature_list = feature.split('+')
-        if "emission:" in feature_list[0]:
-            feature_tuple = (feature_list[1], feature_list[0].replace("emission:",''))
-            if feature_tuple in sequence_pair:
-                if feature in count_dict:
-                    count_dict[feature] += 1
-                else:
-                    count_dict[feature] = 1
+class CRF():
+    def __init__(self):
+        self.training_data = None
+        self.feature_set = None
+        self.tagSet = None
+        self.tagArray = None
+        self.labelCounts = None
+        self.params = None
 
-    return count_dict
+    def getFeature(self):
+        return [[self.feature_set.get_feature_list(word, t) for t in range(len(word))]
+                for word, _ in self.training_data]
+
+    def estimation(self):
+        training_feature_data = self.getFeature()
+        print('Start L-BFGS-B')
+        # Minimize a function func using the L-BFGS-B algorithm.
+        # magic
+        self.params, log_likelihood, information = fmin_l_bfgs_b(
+                                    func=_log_likelihood,
+                                    fprime=_gradient,x0=np.zeros(len(self.feature_set)),
+                                    args=(self.training_data,
+                                    self.feature_set,
+                                    training_feature_data,
+                                    self.feature_set.get_empirical_counts(),
+                                    self.tagSet,
+                                    VARIANCE),
+                                    callback=_callback)
+        print('Finish!')
+
+    #Main function to train model
+    def train(self, inputFilename, model_filename):
+        # Read the training corpus
+        print("Reading data")
+        self.training_data = readTrainFile(inputFilename)
+        print("Reading Finish!")
+
+        # Generate feature set from the input files
+        self.feature_set = FeatureSet()
+        self.feature_set.scan(self.training_data)
+        self.tagSet, self.tagArray = self.feature_set.get_labels()
+        self.labelCounts = len(self.tagArray)
+        print("Number of labels:",self.labelCounts-1)
+        print("Number of features: ",len(self.feature_set))
+
+        # Estimates params to maximize log-likelihood of the input file
+        self.estimation()
+        self.save_model(model_filename)
+
+        print('Training Finished' )
+
+    #Main function to test the input file
+    def test(self, test_inputFilename):
+        test_data = readTestFile(test_inputFilename)
+        with open ('dev.p5.out',encoding='UTF-8',mode='w') as f:
+            for word in test_data:
+                layer=len(word)
+                Yprime = self.inference(word,layer)
+                #Write the predicted result to the file
+                for t in range(len(word)):
+                    f.write(word[t][0])
+                    f.write(' ')
+                    f.write(Yprime[t])
+                    f.write('\n')
+                f.write('\n')
+
+    def save_model(self, model_filename):
+        #model saved as CRF++ format
+        model = {"feature_dic": self.feature_set.serialize_feature_dic(),
+                 "num_features": self.feature_set.num_features,
+                 "labels": self.feature_set.tagArray,
+                 "params": list(self.params)}
+        f = open(model_filename,encoding='utf-8',mode= 'w')
+        json.dump(model, f, ensure_ascii=False, indent=2, separators=(',', ':'))
+        f.close()
+        print('Trained Model saved')
+
+    def inference(self, word,layer):
+        potential_table = generate_potential_table(self.params, self.labelCounts,self.feature_set, word, inference=True)
+        #Use viterbi algorithm to decode the possible sequences
+        Yprime = self.viterbi(word, potential_table,layer)
+        return Yprime
+
+    def viterbi(self, word, potential_table,layer):
+        pi = np.zeros((layer, self.labelCounts))
+        yTable = np.zeros((layer, self.labelCounts), dtype='int64')
+        #Initialization
+        t = 0
+        for index in range(self.labelCounts):
+            pi[t, index] = potential_table[t][StartingIndex, index]
+        #Recursive
+        for t in range(1, layer):
+            for index in range(1, self.labelCounts):
+                maxPi = 0
+                bestLabel = 0
+                for prev_label_id in range(1, self.labelCounts):
+                    value = pi[t-1, prev_label_id] * potential_table[t][prev_label_id, index]
+                    if value > maxPi:
+                        maxPi = value
+                        bestLabel = prev_label_id
+                pi[t, index] = maxPi
+                yTable[t, index] = bestLabel
+        sequence = list()
+        next_label = pi[layer-1].argmax()
+        sequence.append(next_label)
+        #Final Decode
+        for t in range(layer-1, -1, -1):
+            next_label = yTable[t, next_label]
+            sequence.append(next_label)
+
+        return [self.tagSet[index] for index in sequence[::-1][1:]]
+
+    def save_model(self, model_filename):
+        #model saved as CRF++ format
+        model = {"feature_dic": self.feature_set.serialize_feature_dic(),
+                 "num_features": self.feature_set.num_features,
+                 "labels": self.feature_set.tagArray,
+                 "params": list(self.params)}
+        f = open(model_filename,encoding='utf-8',mode= 'w')
+        json.dump(model, f, ensure_ascii=False, indent=2, separators=(',', ':'))
+        f.close()
+        print('Trained CRF Model has been saved')
+
+    def load(self, model_filename):
+        f = open(model_filename,encoding='utf-8',mode='r')
+        model = json.load(f)
+        f.close()
+        self.feature_set = FeatureSet()
+        self.feature_set.load(model['feature_dic'], model['num_features'], model['labels'])
+        self.tagSet, self.tagArray = self.feature_set.get_labels()
+        self.labelCounts = len(self.tagArray)
+        self.params = np.array(model['params'])
+
+        print('CRF model loaded')
 
 
-def calculate_score(sequence_pair):
-    '''
-    a function to calculate the score for a given pair of input and output
-    sequence pair (x_seq, y_seq), based on the above-mentioned features
-    and weights used in Part 1.
+    def feature_function(sequence_pair):
+        '''
+        a function that returns the number of times that the j-th feature in the
+        feature_dictionary appears in sequence pair (x_seq, y_seq).
+        '''
+        count_dict = dict()
+        for feature, weight in feature_dict.items():
+            feature_list = feature.split('+')
+            if "emission:" in feature_list[0]:
+                feature_tuple = (feature_list[1], feature_list[0].replace("emission:",''))
+                if feature_tuple in sequence_pair:
+                    if feature in count_dict:
+                        count_dict[feature] += 1
+                    else:
+                        count_dict[feature] = 1
 
-    '''
-    score_dict = dict()
-    count_dict = feature_function(sequence_pair)
-    for feature, weight in feature_dict.items():
-        if feature in count_dict:
-            score_dict[feature] = weight*count_dict[feature]
-
-    return score_dict
+        return count_dict
 
 
-def viterbi():
-    '''
-    using the Viterbi algorithm to find the most probable output
-    sequence y∗ for an input sequence x.
-    '''
-    pass
+    def calculate_score(sequence_pair):
+        '''
+        a function to calculate the score for a given pair of input and output
+        sequence pair (x_seq, y_seq), based on the above-mentioned features
+        and weights used in Part 1.
+
+        '''
+        score_dict = dict()
+        count_dict = feature_function(sequence_pair)
+        for feature, weight in feature_dict.items():
+            if feature in count_dict:
+                score_dict[feature] = weight*count_dict[feature]
+
+        return score_dict
 
 
 def generate_seqpairs():
